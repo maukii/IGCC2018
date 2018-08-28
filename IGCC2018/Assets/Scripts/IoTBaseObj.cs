2376385aa1a18fd7d6eae6e7f0977617f0fc48e9
Chen Edward TIX17 <edwardchen@kamk.fi>
2018-08-28 15:08:58 +0300

    Added in IoT scripts that work on most objects we're making. All that is needed to do is to change the prop/audio/effect that comes out.

---
+++ IGCC2018/Assets/Scripts/IoTBaseObj.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IoTBaseObj : MonoBehaviour
+{
+    // A toggle to check if its on or off
+    public bool isActivated = false;
+
+    // bool that tells whether object can be hacked or not
+    protected bool canHack = false;
+
+    // Duration of cooldown
+    public float hackCooldownDuration = 1.0f;
+
+    // Cooldown on hack, (1 second?)
+    protected float hackCooldown = 0.0f;
+
+    // How long object stays activated after hack (0.0f = infinite)
+    public float activationDuration = 0.0f;
+
+    // If activation has duration, reduce this one;
+    protected float activationTick = 0.0f;
+
+
+
+	// Use this for initialization
+	void Start()
+    {
+        // nothing, i guess?
+	}
+	
+	// Update is called once per frame
+	virtual protected void Update()
+    {
+        // Prevent rehack until cooldown is up
+        if (hackCooldown > 0.0f)
+        {
+            hackCooldown -= Time.deltaTime;
+            canHack = false;
+        }
+        else
+        {
+            canHack = true;
+        }
+
+        /// Shifted into the objects itself, since some doesn't have activation time
+        //// Shut off object after timer is up
+        //if (isActivated)
+        //{
+        //    if (activationTick > 0.0f)
+        //        activationTick -= Time.deltaTime;
+        //    else
+        //        isActivated = false;
+        //}
+
+        // NOTE: TEMP delete this later
+        if (Input.GetKeyDown("space"))
+        {
+            Hack();
+        }
+    }
+
+    // This called from player when hacking an object
+    virtual protected bool Hack()
+    {
+        if (!canHack)
+            return false;
+
+        return true;
+    }
+}
---
+++ IGCC2018/Assets/Scripts/IoTLight.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IoTLight : IoTBaseObj
+{
+	// Use this for initialization
+	void Start ()
+    {
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+        // Check if there is a activation duration
+        if (activationDuration > 0.0f)
+        {
+            // If active... (Run the disable once)
+            if (isActivated)
+            {
+                if (activationTick > 0.0f)
+                    activationTick -= Time.deltaTime;
+                else
+                {
+                    isActivated = false;
+
+                    gameObject.GetComponent<Light>().intensity = 0.0f;
+                }
+            }
+        }
+    }
+
+    protected override bool Hack()
+    {
+        // Check if cooldown is on
+        if (!base.Hack())
+            return false;
+
+        /// Open hacking minigame (Wishlist)
+        /// If successful, then toggle
+
+        hackCooldown = hackCooldownDuration;
+        activationTick = activationDuration;
+        isActivated = !isActivated;
+
+        // Turn light on/off
+        if (isActivated)
+            gameObject.GetComponent<Light>().intensity = 1.0f;
+        else
+            gameObject.GetComponent<Light>().intensity = 0.0f;
+
+        return true;
+    }
+}
