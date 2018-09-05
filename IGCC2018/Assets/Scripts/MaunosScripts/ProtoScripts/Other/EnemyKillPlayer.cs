8ea24b83956d6769889619f18c7db4110cfc7aa1
maukii <31925951+maukii@users.noreply.github.com>
2018-09-05 14:43:20 +0300

    changes to scripts and level changing anbd audio fixes

---
+++ IGCC2018/Assets/Scripts/MaunosScripts/ProtoScripts/Other/EnemyKillPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyKillPlayer : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    [SerializeField] float minDistance = 0.5f;
+    NavMeshAgent agent;
+
+    Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, target.transform.position) <= minDistance)
+        {
+            agent.isStopped = true;
+
+            if(!TempPlayer.playerIsDead)
+            {
+                TempPlayer.playerIsDead = true;
+                target.GetComponent<TempPlayer>().Die();
+            }
+        }
+    }
+
+    public void Respawn()
+    {
+        agent.isStopped = false;
+        transform.position = startPos;
+    }
+
+}
---
+++ IGCC2018/Assets/Scripts/MaunosScripts/ProtoScripts/Other/FadeBack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeBack : MonoBehaviour
+{
+    public bool fading = false;
+    Renderer rend;
+
+    public float fadeSpeed = 0.05f;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    public void FadeWallBack()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float f = fadeSpeed; f <= 1; f += fadeSpeed)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+        fading = false;
+    }
+
+}
